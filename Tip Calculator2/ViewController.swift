c20347cc49b52596bc3ca7493eec760bf1272d21
Quang Vu <quangvu@Quangs-MacBook-Pro.local>
2017-06-19 21:28:38 -0700

    Initial Commit

---
+++ Tip Calculator2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tip Calculator2
+//
+//  Created by Quang Vu on 6/19/17.
+//  Copyright © 2017 Quang Vu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
